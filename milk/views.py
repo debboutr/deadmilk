48833af223bde26a508af9badc21e7dc5b73dd37
debboutr <debboutr@gmail.com>
2025-09-29 23:19:11 -0700

    sound view wired

--- milk/views.py
+++ milk/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 
 def index(request):
   template = loader.get_template('milk/base.html')
   return HttpResponse(template.render())
+
+def sound(request):
+  return render(request, "milk/sound.html")
