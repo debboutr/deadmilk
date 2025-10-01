ffb9b0ec93746e1eded1f690fb5c9207d94ccfda
debboutr <debboutr@gmail.com>
2025-10-01 14:00:27 -0700

    toggle categories, paint jeapardy

--- milk/models.py
+++ milk/models.py
@@ -1,13 +1,28 @@
 from django.db import models
 
+class CustomQuerySet(models.QuerySet):
+    def odd_sounds(self):
+        return self.annotate(
+                odd=models.F('pk') % 2).filter(odd=True)
+    def even_sounds(self):
+        return self.annotate(
+                odd=models.F('pk') % 2).filter(odd=False)
+
 class SoundCategory(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, unique=True)
     def __str__(self):
         return self.name
 
 class Sound(models.Model):
-    emoji = models.CharField(max_length=30)
+    title = models.CharField(max_length=47, unique=True)
     file = models.FileField(upload_to="uploads/")
-    category = models.ForeignKey(SoundCategory, on_delete=models.CASCADE)
+    category = models.ForeignKey(SoundCategory,
+                                 on_delete=models.CASCADE)
+    objects = CustomQuerySet.as_manager()
     def __str__(self):
         return self.emoji
+    def name(self):
+        return self.file.name.split("/")[1].split(".")[0]
+
+
+
--- milk/views.py
+++ milk/views.py
@@ -1,15 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
-from .models import Sound
+from .models import Sound, SoundCategory
 
 def index(request):
     template = loader.get_template('milk/base.html')
     return HttpResponse(template.render())
 
 def sound(request):
-    sounds = Sound.objects.all()
-    odd_sounds = sounds[::2]
-    even_sounds = sounds[1::2]
-    return render(request, "milk/sound.html", {"odd_sounds": odd_sounds,
-                                               "even_sounds": even_sounds})
+    cats = SoundCategory.objects.all()
+    return render(request, "milk/sound.html", {"cats": cats})
