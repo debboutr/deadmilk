f6fe7cfeb982172879b282a0067519262db63125
debboutr <debboutr@gmail.com>
2025-10-15 10:55:29 -0700

    remove work url

--- milk/urls.py
+++ milk/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("sounds/", views.sound, name="sounds"),
     path("sounds/<slug:category>/", views.sound, name="sounds"),
-    path("work/", views.work, name="work"),
 ]
