80401c364d61df2cbb34f79c13f06a1c490102fd
debboutr <debboutr@gmail.com>
2025-12-14 17:09:36 -0800

    add cookie settings

--- core/settings.py
+++ core/settings.py
@@ -126,3 +126,5 @@ CSRF_TRUSTED_ORIGINS = ["https://deadmilkfarms.com","https://www.deadmilkfarms.c
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = False
